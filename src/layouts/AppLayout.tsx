340da8d8dc72856799c77e680cf15f02a5f3cc8a
martin0123478 <martin_agui97@outlook.es>
2024-12-20 23:01:14 -0600

    feat:add headless ui menu

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 import { Logo } from "@/components/Logo"
+import NavMenu from "@/components/NavMenu"
 
 
 export const AppLayout = () => {
@@ -10,7 +11,9 @@ export const AppLayout = () => {
                     <div className="w-64">
                         <Logo />
                     </div>
+                    <NavMenu />
                 </div>
+
 
             </header>
             <section className="max-w-screen-2xl mx-auto">
