ed025707b9492e63246ba33d3ce0efdf3480f028
martin0123478 <martin_agui97@outlook.es>
2024-12-20 22:41:49 -0600

    feat:create layout

---
+++ src/components/Logo.tsx
@@ -0,0 +1,6 @@
+
+export const Logo = () => {
+    return (
+        <img src="/logo.svg" alt="logotipo" />
+    )
+}
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,11 +1,26 @@
 import { Outlet } from "react-router-dom"
+import { Logo } from "../components/Logo"
 
 
 export const AppLayout = () => {
     return (
         <>
-            <div>AppLayout</div>
-            <Outlet />
+            <header className="bg-gray-800 py-5">
+                <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
+                    <div className="w-64">
+                        <Logo />
+                    </div>
+                </div>
+
+            </header>
+            <section className="max-w-screen-2xl mx-auto">
+                <Outlet />
+            </section>
+            <footer className="py-5 ">
+                <p className="text-center"> Todos los derechos reservados {new Date().getFullYear()}</p>
+
+            </footer>
+
         </>
 
     )
