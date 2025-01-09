94c2e60503a1ac923fc9360ae9dc429f7180789a
martin0123478 <martin_agui97@outlook.es>
2025-01-08 21:53:10 -0600

    feat:create projects with mutation of reactquerry

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify'
 import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 import { ProjectFormData } from "@/types/index";
-
+import { useMutation } from "@tanstack/react-query";
 
 export default function CreateProjectView() {
     const navigate = useNavigate()
@@ -15,12 +15,24 @@ export default function CreateProjectView() {
     }
 
     const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
-    const handleForm = async (formData: ProjectFormData) => {
-        const data = await createProject(formData)
-        toast.success(data)
 
-        navigate('/')
-    }
+    const { mutate } = useMutation({
+        mutationFn: createProject,
+        onError: () => {
+
+        },
+        onSuccess: (data) => {
+            toast.success(data)
+
+            navigate('/')
+        }
+
+
+
+    })
+    const handleForm = (formData: ProjectFormData) => mutate(formData)
+
+
     return (
         <>
             <div className="max-w-3xl mx-auto">
