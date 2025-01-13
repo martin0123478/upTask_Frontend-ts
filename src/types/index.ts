6fe2f263f86a907931f32a24da584a579647a8e4
martin0123478 <martin_agui97@outlook.es>
2025-01-13 17:34:28 -0600

    feat:get all projects, validate wuth zustand and show products in dashboard

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,6 +7,15 @@ export const ProjectSchema = z.object({
     clientName: z.string(),
     description: z.string()
 })
+export const dashBoardProjectSchema = z.array(
+    ProjectSchema.pick({
+        _id: true,
+        projectName: true,
+        clientName: true,
+        description: true
+
+    })
+)
 
 export type Project = z.infer<typeof ProjectSchema>
 export type ProjectFormData = Pick<Project, 'clientName' | 'projectName' | 'description'>
