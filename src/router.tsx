ec44fc45b10aa3bf310293bbc6099a24677c62fe
martin0123478 <martin_agui97@outlook.es>
2025-01-19 20:20:02 -0600

    feat:create route to projects views

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import { AppLayout } from '@/layouts/AppLayout'
 import { DashboardView } from '@/views/DashboardView'
 import CreateProjectView from './views/projects/CreateProjectView'
 import EditProjectView from './views/projects/EditProjectView'
+import { ProjectDetailView } from './views/projects/ProjectDetailView'
 
 export default function Router() {
     return (
@@ -11,6 +12,7 @@ export default function Router() {
                 <Route element={<AppLayout />}>
                     <Route path='/' element={<DashboardView />} index />
                     <Route path='project/create' element={<CreateProjectView />} />
+                    <Route path='project/:projectId' element={<ProjectDetailView />} />
                     <Route path='project/:projectId/edit' element={<EditProjectView />} />
                 </Route>
             </Routes>
---
+++ src/views/projects/ProjectDetailView.tsx
@@ -0,0 +1,7 @@
+
+export const ProjectDetailView = () => {
+    return (
+        <div>ProjectDetailView</div>
+    )
+}
+
