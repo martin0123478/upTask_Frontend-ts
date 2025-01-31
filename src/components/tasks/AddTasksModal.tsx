43d0ed299652c42da05128ff88213d0c2b2e3fe7
martin0123478 <martin_agui97@outlook.es>
2025-01-31 17:42:51 -0600

    feat:create task with useMutation

--- src/components/tasks/AddTasksModal.tsx
+++ src/components/tasks/AddTasksModal.tsx
@@ -1,17 +1,26 @@
 import { Fragment } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import TaskForm from './FormTask';
 import { useForm } from 'react-hook-form';
 import { TaskFormData } from '@/types/index';
+import { useMutation } from '@tanstack/react-query';
+import { createTask } from '@/api/TaskAPI';
+import { toast } from 'react-toastify';
 
 
 export default function AddTaskModal() {
     const navigate = useNavigate()
+    //Leer si modal existe
     const location = useLocation()
     const queryParams = new URLSearchParams(location.search)
     const modalTask = queryParams.get('newTask')
     const show = modalTask ? true : false
+
+    //Obtener projectId
+    const params = useParams()
+    const projectId = params.projectId!
+    console.log(projectId)
 
     const initialValues: TaskFormData = {
         name: '',
@@ -19,11 +28,26 @@ export default function AddTaskModal() {
 
     }
 
-    const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
+    const { register, handleSubmit, reset, formState: { errors } } = useForm({ defaultValues: initialValues })
+    const { mutate } = useMutation({
+        mutationFn: createTask,
+        onError: (error) => {
+            toast.error(error.message)
+        },
+        onSuccess: (data) => {
+            toast.success(data)
+            reset()
+            navigate(location.pathname, { replace: true })
+
+        }
+    })
 
     const handleCreateTask = (formData: TaskFormData) => {
-        console.log(formData)
+        const data = { formData, projectId }
+        mutate(data)
     }
+
+
 
     return (
         <>
