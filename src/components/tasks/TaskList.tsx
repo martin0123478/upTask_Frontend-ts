5cdf2c11c681e399bdad5d3538dc51172dda6c8a
martin0123478 <martin_agui97@outlook.es>
2025-01-31 20:03:46 -0600

    feat:add colors to diferents status

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -23,6 +23,14 @@ const statusTrnslations: { [key: string]: string } = {
     completed: 'Completado'
 }
 
+const statusColors: { [key: string]: string } = {
+    pending: 'border-t-slate-500',
+    onHold: 'border-t-red-500',
+    inProgress: 'border-t-blue-500',
+    underReview: 'border-t-amber-500',
+    completed: 'border-t-emerald-500'
+}
+
 export const TaskList = ({ tasks }: TaskListProps) => {
     const groupedTasks = tasks.reduce((acc, task) => {
         let currentGroup = acc[task.status] ? [...acc[task.status]] : [];
@@ -39,11 +47,11 @@ export const TaskList = ({ tasks }: TaskListProps) => {
                 {Object.entries(groupedTasks).map(([status, tasks]) => (
                     <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
                         <h3
-                            className='capitalize text-xl
+                            className={`capitalize text-xl
                          font-light border
-                          border-slate-300
+                          ${statusColors[status]}
                            bg-white p-3 
-                           border-t-8'>{statusTrnslations[status]}</h3>
+                           border-t-8`}>{statusTrnslations[status]}</h3>
                         <ul className='mt-5 space-y-5'>
                             {tasks.length === 0 ? (
                                 <li className="text-gray-500 text-center pt-3">No Hay tareas</li>
