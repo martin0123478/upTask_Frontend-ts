74e943d67212ee2d4988531e54c9cbe98fd5296e
martin0123478 <martin_agui97@outlook.es>
2025-01-31 19:35:13 -0600

    feat:show state of task

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -1,5 +1,4 @@
 import { Task } from '@/types/index'
-import React from 'react'
 import { TaskCard } from './TaskCard'
 
 type TaskListProps = {
@@ -15,6 +14,13 @@ const initialStatusGroup: GroupTask = {
     underReview: [],
     completed: []
 
+}
+const statusTrnslations: { [key: string]: string } = {
+    pending: 'Pendiente',
+    onHold: 'En espera',
+    inProgress: 'En progreso',
+    underReview: 'En revision',
+    completed: 'Completado'
 }
 
 export const TaskList = ({ tasks }: TaskListProps) => {
@@ -32,6 +38,12 @@ export const TaskList = ({ tasks }: TaskListProps) => {
             <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
                 {Object.entries(groupedTasks).map(([status, tasks]) => (
                     <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
+                        <h3
+                            className='capitalize text-xl
+                         font-light border
+                          border-slate-300
+                           bg-white p-3 
+                           border-t-8'>{statusTrnslations[status]}</h3>
                         <ul className='mt-5 space-y-5'>
                             {tasks.length === 0 ? (
                                 <li className="text-gray-500 text-center pt-3">No Hay tareas</li>
